a071dd3af89f9a788dc7bce4af7c9c00c8451b59
rizzles <mike@itchycats.com>
2011-08-22 18:52:28 +0000

    fix to trend

--- market.py
+++ market.py
@@ -171,7 +171,7 @@ class TrendHandler(BaseHandler):
         trend = db.get("""SELECT * FROM trends.trends WHERE uuid = %s""", uuid)
         
         # zoomed in version
-        ticks = db.query("""SELECT date,open,close,high,low,id from %s where date >= %s order by date"""% (trend['symbol'], int(trend['startdate']-20000)))
+        ticks = db.query("""SELECT date,open,close,high,low,id from %s where date >= %s order by date"""% (trend['symbol'], int(trend['startdate']-2000000)))
 #        ticks = db.query("""SELECT date,open,close,high,low,id from %s where date >= %s order by date"""% (trend['symbol'], int(trend['date'])-20))
 
         p1 = db.get("""SELECT * FROM %s WHERE date = %s"""% (trend['symbol'], trend['p1']))
