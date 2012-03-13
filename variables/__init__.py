dbf070d749b9fdd3412cea8413445707c67651e4
Ubuntu <ubuntu@ip-10-212-66-144.ec2.internal>
2012-03-13 16:56:02 +0000

    not sure

--- variables/__init__.py
+++ variables/__init__.py
@@ -7,7 +7,6 @@ import tornado.database
 define("port", default=80, help="run on the given port", type=int)
 
 db = tornado.database.Connection(
-#    host='184.73.152.25:3307', database='ticks',
-    host='10.212.66.144:3307', database='ticks',
+    host='10.98.49.126:3307', database='ticks',
     user='nick', password='mohair94')
 
